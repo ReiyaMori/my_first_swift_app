a826a8534f6ac0e055347f1b2bfb89bbb67019e7
ReiyaMori <hanedab787@gmail.com>
2021-04-22 08:02:49 +0900

    first commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by 森伶也 on 2021/04/22.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
